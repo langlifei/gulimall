ed8f2a484fbbef43ce120a3b3e7909cf63d8a83f
langlifei <1084327512@qq.com>
2021-10-20 22:59:57 +0800

    添加查询服务的DSL查询语句

--- gulimall-search/src/main/java/com/atguigu/gulimall/lsearch/constant/EsConstant.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/lsearch/constant/EsConstant.java
@@ -2,5 +2,5 @@ package com.atguigu.gulimall.lsearch.constant;
 
 public class EsConstant {
 
-    public static final String PRODUCT_INDEX = "product";
+    public static final String PRODUCT_INDEX = "gulimall_product";
 }
