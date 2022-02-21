18985adc0d5da3861c5a229b3c21b217cfc90d6b
langlifei <1084327512@qq.com>
2022-02-21 22:53:37 +0800

    1.完成社交登录中的微博登录,由于微博开发者需要审核才能使用微博登录,故未对其功能进行测试 进度:225、商城业务-认证服务-分布式session不共享不同步问题

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/controller/MemberController.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import com.atguigu.gulimall.member.exception.UsernameException;
 import com.atguigu.gulimall.member.feign.CouponFeignService;
 import com.atguigu.gulimall.member.vo.MemberUserLoginVo;
 import com.atguigu.gulimall.member.vo.MemberUserRegisterVo;
+import com.atguigu.gulimall.member.vo.SocialUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -55,6 +56,18 @@ public class MemberController {
     public R login(@RequestBody MemberUserLoginVo vo) {
 
         MemberEntity memberEntity = memberService.login(vo);
+
+        if (memberEntity != null) {
+            return R.ok().setData(memberEntity);
+        } else {
+            return R.error(BizCodeEnume.LOGINACCT_PASSWORD_EXCEPTION.getCode(),BizCodeEnume.LOGINACCT_PASSWORD_EXCEPTION.getMsg());
+        }
+    }
+
+    @PostMapping(value = "/oauth2/login")
+    public R oauthLogin(@RequestBody SocialUser socialUser) throws Exception {
+
+        MemberEntity memberEntity = memberService.login(socialUser);
 
         if (memberEntity != null) {
             return R.ok().setData(memberEntity);
--- gulimall-member/src/main/java/com/atguigu/gulimall/member/service/impl/MemberServiceImpl.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/service/impl/MemberServiceImpl.java
@@ -1,5 +1,8 @@
 package com.atguigu.gulimall.member.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.common.utils.HttpUtils;
 import com.atguigu.gulimall.member.dao.MemberLevelDao;
 import com.atguigu.gulimall.member.entity.MemberLevelEntity;
 import com.atguigu.gulimall.member.exception.PhoneException;
@@ -7,10 +10,13 @@ import com.atguigu.gulimall.member.exception.UsernameException;
 import com.atguigu.gulimall.member.vo.MemberUserLoginVo;
 import com.atguigu.gulimall.member.vo.MemberUserRegisterVo;
 import com.atguigu.gulimall.member.vo.SocialUser;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -71,13 +77,64 @@ public class MemberServiceImpl extends ServiceImpl<MemberDao, MemberEntity> impl
 
     @Override
     public MemberEntity login(SocialUser socialUser) throws Exception {
-        return null;
+
+        //具有登录和注册逻辑
+        String uid = socialUser.getUid();
+
+        //1、判断当前社交用户是否已经登录过系统
+        MemberEntity memberEntity = this.baseMapper.selectOne(new QueryWrapper<MemberEntity>().eq("social_uid", uid));
+
+        if (memberEntity != null) {
+            //这个用户已经注册过
+            //更新用户的访问令牌的时间和access_token
+            MemberEntity update = new MemberEntity();
+            update.setId(memberEntity.getId());
+            update.setAccessToken(socialUser.getAccess_token());
+            update.setExpiresIn(socialUser.getExpires_in());
+            this.baseMapper.updateById(update);
+
+            memberEntity.setAccessToken(socialUser.getAccess_token());
+            memberEntity.setExpiresIn(socialUser.getExpires_in());
+            return memberEntity;
+        } else {
+            //2、没有查到当前社交用户对应的记录我们就需要注册一个
+            MemberEntity register = new MemberEntity();
+            //3、查询当前社交用户的社交账号信息（昵称、性别等）
+            Map<String,String> query = new HashMap<>();
+            query.put("access_token",socialUser.getAccess_token());
+            query.put("uid",socialUser.getUid());
+            HttpResponse response = HttpUtils.doGet("https://api.weibo.com", "/2/users/show.json", "get", new HashMap<String, String>(), query);
+
+            if (response.getStatusLine().getStatusCode() == 200) {
+                //查询成功
+                String json = EntityUtils.toString(response.getEntity());
+                JSONObject jsonObject = JSON.parseObject(json);
+                String name = jsonObject.getString("name");
+                String gender = jsonObject.getString("gender");
+                String profileImageUrl = jsonObject.getString("profile_image_url");
+
+                register.setNickname(name);
+                register.setGender("m".equals(gender)?1:0);
+                register.setHeader(profileImageUrl);
+                register.setCreateTime(new Date());
+                register.setSocialUid(socialUser.getUid());
+                register.setAccessToken(socialUser.getAccess_token());
+                register.setExpiresIn(socialUser.getExpires_in());
+
+                //把用户信息插入到数据库中
+                this.baseMapper.insert(register);
+
+            }
+            return register;
+        }
+
     }
 
     @Override
     public MemberEntity login(String accessTokenInfo) {
         return null;
     }
+
 
     @Override
     public void register(MemberUserRegisterVo vo) {
